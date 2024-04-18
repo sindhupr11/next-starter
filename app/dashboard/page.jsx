d8e2bf2c7c1928197c49a045ef4a6388e051a55d
Mishal <mishal0404@gmail.com>
2024-04-19 04:39:57 +0530

    cors headers on api

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,22 +1,42 @@
-import { Container, Typography } from "@mui/material";
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options"
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
+
+async function getProjects(acesstoken) {
+  
+  const res = await fetch(`http://localhost:3000/backend/python`,
+  {
+    headers: {
+      Authorization: `Bearer ${acesstoken}`,
+    },
+    cache: 'no-store'
+  })
+  const projects = await res.json()
+  console.log(projects)
+  return projects.message
+}
+
 export default async function Dashboard() {
   const session =  await getServerSession(options)
-  
+
+  const projects = await getProjects(session.acesstoken)
+
   return (
-  <Container sx={{justifyContent:"center", display:"flex", alignItems:"center", alignContent:"center", height:"100vh", flexDirection:"column"}}>
-    <Typography component="h1" variant="h2" sx={{paddingBottom:"2em"}}>
+  <div style={{justifyContent:"center", display:"flex", alignItems:"center", alignContent:"center", height:"100vh", flexDirection:"column"}}>
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl m-5">
             Hi {session.user.name}<br/>
-    </Typography>
+    </h1>
+
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight m-10 mt-5">
+      {projects}
+    </h3>
     
     <Button variant="outline" asChild style={{backgroundColor:"white", color:"black"}}>
     <Link href="/api/auth/signout" >Sign Out</Link>
     </Button>
 
     
-  </Container>
+  </div>
   )
 }
--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Define your domain
+allowed_domains = ["http://localhost:3000"]  # Replace with your actual domain
+
+# CORS middleware to allow only requests from the allowed domain
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_domains,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/backend/python")
-def hello_world():
+def hello_world(request: Request):
+    origin = request.headers.get("Origin")
+    print(request.headers)
+    if origin not in allowed_domains:
+        raise HTTPException(status_code=403, detail="Forbidden")
     return {"message": "Hello World"}
