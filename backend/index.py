d72406e0b8c11b4631901ca34cd8d28bbfd3fc91
Mishal <mishal0404@gmail.com>
2024-04-19 09:41:23 +0530

    bug fixing

--- backend/index.py
+++ backend/index.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Define your domain
-allowed_domains = ["http://localhost:3000", "127.0.0.1:8000", "next-starter-swart.vercel.app"]  # Replace with your actual domain
+allowed_domains = ["localhost:3000", "127.0.0.1:8000", "next-starter-swart.vercel.app"]  # Replace with your actual domain
 
 # CORS middleware to allow only requests from the allowed domain
 app.add_middleware(
@@ -18,8 +18,4 @@ app.add_middleware(
 
 @app.get("/backend/python")
 def hello_world(request: Request):
-    host = request.headers.get("host")
-    print(request.headers)
-    if host != allowed_domains:
-        raise HTTPException(status_code=403, detail="Forbidden")
-    return {"message": "Hello World"}
+    return request.headers
